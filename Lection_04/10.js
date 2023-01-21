0179e77f5971decf5f1f6a98dae11dcfced80d01
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-01-21 13:51:45 +0200

    Update 10.js

--- Lection_04/10.js
+++ Lection_04/10.js
@@ -1,18 +1,24 @@
 function spiceMustFlow(startingYield) {
-  let yield = startingYield - 26;
+  let yield = 0;
+  let smallYield = startingYield;
   let days = 0;
+
   while (startingYield >= 100) {
+    yield += startingYield - 26;
     startingYield -= 10;
-    if (startingYield >= 100) {
-      yield += startingYield - 26;
-    } else {
-      days++;
-      break;
-    }
     days++;
   }
-  yield -= 26;
+  if (smallYield < 26) {
+    yield = 0;
+  } else {
+    if (yield > 0) {
+      yield = yield - 26;
+    } else {
+      yield = smallYield - 26;
+      days++;
+    }
+  }
   console.log(days);
   console.log(yield);
 }
-spiceMustFlow(450);
+spiceMustFlow(25);
